77f9ac343732967b01f36a467d07a8da195b2653
Christian Catalan <crcatala@gmail.com>
2018-10-06 12:57:22 -0500

    Add logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="Home app-content-wrapper">
+    <div>
+      <img alt="App Logo" src="@/assets/app-logo.png" class='Home__app-logo'>
+    </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
+  name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
   }
 };
 </script>
+
+<style lang='scss' scoped>
+.Home {
+  display: flex;
+  justify-content: center;
+
+  &__app-logo {
+    width: 200px;
+  }
+}
+</style>
+
